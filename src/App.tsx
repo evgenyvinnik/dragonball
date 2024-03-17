ac63e6e5cf4ea9d9d9207fd04937562d59fadf39
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-16 23:17:08 -0700

    Getting closer!

--- src/App.tsx
+++ src/App.tsx
@@ -22,26 +22,43 @@ const center: google.maps.LatLngLiteral = {
   lng: 80.6337262, // default longitude
 };
 
+const otherBallsCount = 6;
+const maxTries = 10;
+const maxMoves = 30;
+
 function App() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyBMid2MJczEm0Y_8P5HgCkSrw-iev-EtX0",
     libraries: googleMapsLibraries,
   });
 
-  const [marker, setMarker] = useState<google.maps.LatLngLiteral | undefined>(
-    undefined
-  );
+  // const [marker, setMarker] = useState<google.maps.LatLngLiteral | undefined>(
+  //   undefined
+  // );
+
+  const [markers, setMarkers] = useState<
+    google.maps.LatLngLiteral[] | undefined
+  >(undefined);
 
   const [message, setMessage] = useState("");
 
   const [open, setOpen] = useState(false);
 
+  const addRandomMarkers = () => {
+    for (let i = 0; i < otherBallsCount; i++) {}
+  };
+
+  const moveMarkers = () => {};
+
   const onMapClick = (e: google.maps.MapMouseEvent) => {
     if (e.latLng != null) {
-      setMarker({
+      let newMarkers: google.maps.LatLngLiteral[] = [];
+      let newMarker = {
         lat: e.latLng.lat(),
         lng: e.latLng.lng(),
-      });
+      };
+      newMarkers.push(newMarker);
+      setMarkers(newMarkers);
       setMessage(
         `Set marker at latitude: ${e.latLng.lat()} and longitude: ${e.latLng.lng()} `
       );
@@ -90,19 +107,22 @@ function App() {
           center={center}
           onClick={onMapClick}
         >
-          {marker ? (
-            <Marker
-              position={{
-                lat: marker.lat,
-                lng: marker.lng,
-              }}
-              icon={{
-                url: require("./assets/Dragonball-1.ico"),
-                fillColor: "#EB00FF",
-                scale: 7,
-              }}
-            />
-          ) : null}
+          {markers
+            ? markers.map((marker, index) => (
+                <Marker
+                  key={index}
+                  position={{
+                    lat: marker.lat,
+                    lng: marker.lng,
+                  }}
+                  icon={{
+                    url: require("./assets/Dragonball-1.ico"),
+                    fillColor: "#EB00FF",
+                    scale: 7,
+                  }}
+                />
+              ))
+            : null}
         </GoogleMap>
       </Box>
       <Snackbar
