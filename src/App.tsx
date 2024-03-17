1e791443886e950ac2a5cfc4be28d36103b7b470
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-16 22:56:23 -0700

    added clicking and showing snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ import {
   Marker,
   LoadScriptProps,
 } from "@react-google-maps/api";
+import Snackbar from "@mui/material/Snackbar";
+import Box from "@mui/material/Box";
+import { AppBar, Toolbar } from "@mui/material";
 
 const googleMapsLibraries: LoadScriptProps["libraries"] = ["places"];
 
 const mapContainerStyle = {
-  width: "100vw",
-  height: "100vh",
+  width: "100%",
+  height: "100%",
 };
 const center: google.maps.LatLngLiteral = {
   lat: 7.2905715, // default latitude
@@ -29,12 +32,20 @@ function App() {
     undefined
   );
 
+  const [message, setMessage] = useState("");
+
+  const [open, setOpen] = useState(false);
+
   const onMapClick = (e: google.maps.MapMouseEvent) => {
     if (e.latLng != null) {
       setMarker({
         lat: e.latLng.lat(),
         lng: e.latLng.lng(),
       });
+      setMessage(
+        `Set marker at latitude: ${e.latLng.lat()} and longitude: ${e.latLng.lng()} `
+      );
+      setOpen(true);
     }
   };
 
@@ -46,29 +57,60 @@ function App() {
     return <div>Loading maps</div>;
   }
 
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={2}
-        center={center}
-        onClick={onMapClick}
+    <div className="App">
+      <AppBar position="sticky">
+        <Toolbar>
+          Dragonball placement tool: click on the land to place your first
+          Dragonball
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "#cfe8fc",
+          height: "100%",
+        }}
       >
-        {marker ? (
-          <Marker
-            position={{
-              lat: marker.lat,
-              lng: marker.lng,
-            }}
-            icon={{
-              // path: google.maps.SymbolPath.CIRCLE,
-              url: require("./assets/Dragonball-1.ico"),
-              fillColor: "#EB00FF",
-              scale: 7,
-            }}
-          />
-        ) : null}
-      </GoogleMap>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={2}
+          center={center}
+          onClick={onMapClick}
+        >
+          {marker ? (
+            <Marker
+              position={{
+                lat: marker.lat,
+                lng: marker.lng,
+              }}
+              icon={{
+                url: require("./assets/Dragonball-1.ico"),
+                fillColor: "#EB00FF",
+                scale: 7,
+              }}
+            />
+          ) : null}
+        </GoogleMap>
+      </Box>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        message={message}
+      />
     </div>
   );
 }
